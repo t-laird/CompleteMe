8fa79a90642c5a260694996e3970ddd41e9e2d71
Thomas Laird <t6r6l5@gmail.com>
2017-10-21 00:13:13 -0600

    add tests for smarter suggestions

--- test/Trie-test.js
+++ test/Trie-test.js
@@ -1,18 +1,37 @@
 import Trie from '../lib/Trie.js';
 import { expect, assert } from 'chai';
+import fs from 'fs';
 const text = "/usr/share/dict/words";
+const dictionary = fs.readFileSync(text).toString().trim().split('\n')
 
-
-describe('Trie', function (){
+describe('CompleteMe', function (){
   let completeMe = new Trie();
 
-  it ('Should have an item after using the insert method', function (){
+  it('Should be able to suggest words based upon a query', function (){
     completeMe.insert('pizza');
+    // console.log(Object.entries(completeMe.linkedList['children']['p']['children']['i']['children']['z']['children']['z']['children']['a']));
     expect(completeMe.count()).to.equal(1);
+    // expect(completeMe.suggest('bad')).to.deep.equal([]);
+    // expect(completeMe.suggest('piz')).to.deep.equal(['pizza']);
   });
 
-  it ('Should have two items after inserting a second item' , function(){
-    completeMe.insert('apple');
-    expect(completeMe.count()).to.equal(2);
+  it('Should have 235,886 items after using the populate method', function (){
+    completeMe = new Trie();
+    completeMe.populate(dictionary);
+    expect(completeMe.count()).to.equal(235886);
+    // console.log(Object.entries(completeMe.linkedList['children']['c']['children']['h']['children']['i']['children']['l']['children']['d']['children']['w']['children']['a']['children']['r']));
+  });
+
+  it('Should offer suggestions based upon the first several characters entered', function () {
+    completeMe.suggest('pizz');
+    expect(completeMe.suggestions).to.deep.equal(['pizza','pizzeria','pizzicato','pizzle']);
+  });
+  
+  it('Should increase the priority of a suggestion after its being selected', function () {
+    completeMe.select('pizzeria');
+    completeMe.select('pizzeria');
+    completeMe.suggest('pizz');
+    expect(completeMe.suggestions).to.deep.equal(['pizzeria','pizza','pizzicato','pizzle']);
+
   });
 });
